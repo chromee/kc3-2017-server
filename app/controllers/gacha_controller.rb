89ccb004d048cccf2ff248bdc8ef324b212367a9
chromee <odk.mmmm@gmail.com>
2017-09-12 00:23:28 +0900

    executeでUserCardを保存するように

--- app/controllers/gacha_controller.rb
+++ app/controllers/gacha_controller.rb
@@ -1,8 +1,16 @@
 class GachaController < ApplicationController
+  before_action :set_user
+
   def top
   end
 
   def execute
     @card = Card.all.sample
+    UserCard.create(user_id: @user.id, card_id: @card.id)
+  end
+
+  private
+  def set_user
+    @user = User.find_by_id(params[:user_id].to_i)
   end
 end
